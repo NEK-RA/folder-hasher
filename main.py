ebb34e7cdb13573395065b31443e661ac9b357d2
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2022-10-27 18:06:17 +0300

    recursive for files to calculate hashsum seems to be working

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from pathlib import Path
 # depending on passed value
 # made to let user choose target hashing algorithm
 # currently made without match-case to make script work in older versions
-def gethasher(method):
+def get_hasher(method):
   if method == 'sha1':
     return hashlib.sha1()
   elif method == 'sha256':
@@ -24,7 +24,7 @@ def gethasher(method):
 # take required hashing algorithm, open target file for reading in binary mode
 # returning hash value in format similar to seen by using md5sum and etc
 # need to edit path to avoid listing unnecessary parts of filepaths
-def hashfile(path, method):
+def hash_file(path, method):
   hashing = gethasher(method)
   with open("main.py","rb") as source:
     file = source.read()
@@ -32,6 +32,45 @@ def hashfile(path, method):
     result = hashing.hexdigest()
     return f"{result}  {path.resolve()}"
 
+def recursive_childs(path,level=0):
+  paths = []
+  files = []
+  folders = []
+  if path.is_dir():
+    for item in list(path.iterdir()):
+      if item.is_file():
+        files.append(item)
+      else:
+        folders.append(item)
+  else:
+    files.append(path)
+  if len(files):
+    paths.append(
+      dict(
+        depth=level,
+        tasks=files
+      )
+    )
+    
+  deeper = []
+  for item in folders:
+    deeper = recursive_childs(item,level+1)
+    for deepitem in deeper:
+      if len(deepitem['tasks']):
+        paths.append(deepitem)
+  # print(deeper)
+  # print(paths)
+  return paths
+    
 
-path = Path("main.py")
-print( hashfile(path,"sha1") )
+
+path = Path("/usr/share/wallpapers")
+# path = Path("/usr/lib/dpkg/methods/apt")
+# path = Path("/usr/lib/dpkg/")
+sets = recursive_childs(path)
+for each in sets:
+  print(each)
+  for task in each["tasks"]:
+    print(task)
+  print("\n")
+#print( hashfile(path,"sha1") )
