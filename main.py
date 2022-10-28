c4edbfd127328ed64f9cafe5ca657c5eb51cf19e
Ryoidenshi Aokigahara <ryoidenshiaokigahara@gmail.com>
2022-10-28 21:50:46 +0300

    everything prepared to write into files

--- main.py
+++ main.py
@@ -24,18 +24,20 @@ def get_hasher(method):
 # file hashing function
 def hash_file(path, method):
   # take required hashing algorithm
-  hashing = gethasher(method)
+  hashing = get_hasher(method)
   # open target file for reading in binary mode
-  with open("main.py","rb") as source:
+  with path.open(mode="rb") as source:
     file = source.read()
     hashing.update(file)
     result = hashing.hexdigest()
     # returning hash value in format similar to produced by md5sum and etc (hash, 2 spaces, filename)
     # need to edit path to files to avoid listing unnecessary parts of filepaths (start/file instead of /home/user/downloads/start/file)
     # the idea for now is to pass starting point path to hash_file function
-    return f"{result}  {path.resolve()}"
+    return result
     
 # function to collect list of files for which hashsum should be calculated
+# it create list of dicts with raw tasks
+# later this list of raw tasks will be processed in hash_tasks function
 def recursive_childs(path,depth,level=0):
   # final list of tasks - dict(depth=number, tasks=[list,of,paths])
   paths = []
@@ -70,8 +72,6 @@ def recursive_childs(path,depth,level=0):
           # then all nested paths should be inside of tasks for this level
           else:
             files.extend(deepitem["tasks"])
-          
-
   # adding files from current folder
   if len(files):
     paths.insert(
@@ -82,20 +82,62 @@ def recursive_childs(path,depth,level=0):
       )
     )
   # returning list of tasks in current folder
-  
   return paths
-    
-def hash_tasks(tasksets):
+
+def final_preparation(path,start,level,absolute):
+  levels = str(path).split("/")
+  index = levels.index(start)
+  cut = None
+  # checking if paths on required level are folders, not files
+  # (actually if it's not the last element of path)
+  # and selecting place to split by this
+  if(index+1 != len(levels)-1):
+    cut = index+1+level
+  else:
+    cut = index+1
+  # getting part which will be folder where hashfile should be located
+  hashfile = "/".join(levels[:cut])
+  # adding folder name one more time, as it will be hashfile name actually
+  # extension will be added to it in hash_tasks function, right before writing hashes to file
+  hashfile += f"/{levels[cut-1]}"
+  # relative path to file, which should be written to hashfile
+  # if absolute=true, then absolute path will be kept for file
+  # otherwise relative will be used
+  relative = ""
+  if absolute:
+    relative = str(path)
+  else:
+    relative = "/".join(levels[cut:])
+  # returning path too, as it will be used to pass to hash function
+  return (hashfile, relative, path)
+  
+
+def hash_tasks(tasksets,startpath,method,absolute):
+  final_tasks = dict()
   for taskset in tasksets:
-    print(taskset)
-    print("\n")
+    for task in taskset["tasks"]:
+      hashfile, relative, path = final_preparation(task,startpath,taskset["depth"],absolute)
+      if hashfile not in final_tasks.keys():
+        final_tasks[hashfile] = [dict(name=relative,fullpath=path)]
+      else:
+        final_tasks[hashfile].append(dict(name=relative,fullpath=path))
+
+  for key in final_tasks.keys():
+    content = ""
+    hashfilepath = f"{key}.{method}"
+    for file in final_tasks[key]:
+      checksum = hash_file(file["fullpath"],method)
+      content += f"{checksum}  {file['name']}\n"
+    print(f"\n\nGoing to write into {hashfilepath} next content:\n{content}")
 
 # pathstr = "/usr/share/wallpapers"
 # pathstr = "/usr/lib/dpkg/methods/apt"
-pathstr = "/usr/lib/dpkg/"
+# pathstr = "/usr/lib/dpkg/"
+pathstr = "~/deta-template"
 if "~" in pathstr:
   pathstr = pathstr.replace("~",str(Path.home()))
 path = Path(pathstr).resolve()
-sets = recursive_childs(path,0)
-hash_tasks(sets)
+sets = recursive_childs(path,3)
+print(f"Hashing folder: {pathstr}\nAlgorithm: md5\nUsing depth: 3\nUsing absolute paths: False")
+hash_tasks(sets, path.name, "md5",True)
 #print( hashfile(path,"sha1") )
